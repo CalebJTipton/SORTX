90a88ed9947b25d944a97c68f4167fcdf0446507
CalebJTipton <caleb@tipton.cloud>
2023-01-26 19:31:09 -0600

    created basic file browsing

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.browseDirectory_btn = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.directoryPath_txt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // browseDirectory_btn
+            // 
+            this.browseDirectory_btn.Location = new System.Drawing.Point(420, 64);
+            this.browseDirectory_btn.Name = "browseDirectory_btn";
+            this.browseDirectory_btn.Size = new System.Drawing.Size(75, 23);
+            this.browseDirectory_btn.TabIndex = 0;
+            this.browseDirectory_btn.Text = "Browse";
+            this.browseDirectory_btn.UseVisualStyleBackColor = true;
+            this.browseDirectory_btn.Click += new System.EventHandler(this.browseDirectory_btn_Click);
+            // 
+            // directoryPath_txt
+            // 
+            this.directoryPath_txt.Location = new System.Drawing.Point(207, 65);
+            this.directoryPath_txt.Name = "directoryPath_txt";
+            this.directoryPath_txt.Size = new System.Drawing.Size(190, 23);
+            this.directoryPath_txt.TabIndex = 1;
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 571);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(714, 571);
+            this.Controls.Add(this.directoryPath_txt);
+            this.Controls.Add(this.browseDirectory_btn);
+            this.Name = "MainWindow";
             this.Text = "SORTX V0.0.1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button browseDirectory_btn;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox directoryPath_txt;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -6,5 +6,15 @@ namespace SORTX
         {
             InitializeComponent();
         }
+
+        String directoryPath;
+        private void browseDirectory_btn_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                directoryPath = folderBrowserDialog1.SelectedPath; //updates the directoryPath variable with dialog selection
+                directoryPath_txt.Text = directoryPath; //updates the directoryPath textbok with the new dialog selection
+            }
+        }
     }
 }
