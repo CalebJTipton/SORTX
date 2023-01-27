cc74c036a871056fc832bf4c5990652717243744
CalebJTipton <caleb@tipton.cloud>
2023-01-26 19:17:49 -0600

    updating naming scheme and starting base for development

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,8 +1,8 @@
 namespace SORTX
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
